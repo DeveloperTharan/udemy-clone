5a6ea89971504cb4f7420fa32af66ba3e44d0bf4
Dharanitharan <thxran3@gmail.com>
2024-01-07 09:32:20 +0000

    commit

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,30 +1,49 @@
 "use client";
 
 import React from "react";
+
 import { Tabs, Tab } from "@nextui-org/react";
+
+import {
+  Aperture,
+  CandlestickChart,
+  Clapperboard,
+  Cpu,
+  Dumbbell,
+  Laptop2,
+  Music,
+} from "lucide-react";
 
 const Category = () => {
   const TabMenu = [
-    { id: 1, title: "Finance" },
-    { id: 2, title: "Computer Science" },
-    { id: 3, title: "Enginering" },
-    { id: 4, title: "Filming" },
-    { id: 5, title: "Fitness" },
-    { id: 6, title: "Music" },
-    { id: 7, title: "Photography" },
+    { id: 1, title: "Finance", icon: <CandlestickChart className="h-4 w-4 text-green-600" /> },
+    { id: 2, title: "Computer Science", icon: <Laptop2 className="h-4 w-4 text-blue-600" /> },
+    { id: 3, title: "Enginering", icon: <Cpu className="h-4 w-4 text-red-600" /> },
+    { id: 4, title: "Filming", icon: <Clapperboard className="h-4 w-4 text-purple-600" /> },
+    { id: 5, title: "Fitness", icon: <Dumbbell className="h-4 w-4 text-red-600" /> },
+    { id: 6, title: "Music", icon: <Music className="h-4 w-4 text-pink-600" /> },
+    { id: 7, title: "Photography", icon: <Aperture className="h-4 w-4 text-blue-600" /> },
   ];
 
   return (
     <div className="flex w-full flex-col my-2">
       <Tabs variant={"light"} aria-label="Tabs variants" fullWidth>
         {TabMenu.map((items) => (
-          <Tab key={items.id} title={items.title}>
+          <Tab
+            key={items.id}
+            title={
+              <div className="flex items-center space-x-2">
+                {items.icon}
+                <span>{items.title}</span>
+              </div>
+            }
+          >
             <div className="">{items.title}</div>
           </Tab>
         ))}
       </Tabs>
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
