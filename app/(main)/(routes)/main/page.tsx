ff95f84b07051dabd54acd159c8ed95e9abe2c43
Dharanitharan <dharanitharan137@gmail.com>
2024-01-13 07:09:59 +0000

    commit

--- app/(main)/(routes)/main/page.tsx
+++ app/(main)/(routes)/main/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 import Category from "@/components/Category";
+import { db } from "@/lib/db";
 
-export default function Main() {
+export default async function Main() {
+  const category = await db.category.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  });
+
   return (
     <div className="flex flex-col w-full h-auto min-h-screen px-5">
-      <Category />
+      <Category category={category} />
     </div>
   );
 }
+
