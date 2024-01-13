7b874275a930163dc1c77ef39014a8261688096e
Dharanitharan <dharanitharan137@gmail.com>
2024-01-13 07:29:32 +0000

    .

--- components/Category.tsx
+++ components/Category.tsx
@@ -15,7 +15,15 @@ import {
 } from "lucide-react";
 import { Category } from "@prisma/client";
 
-
+const IconMap: Record<Category["name"], JSX.Element> = {
+  "Computer Science": <Laptop2 size={17} className="text-violet-500" />,
+  "Engineering": <Cpu size={17} className="text-red-500" />,
+  "Finance": <CandlestickChart size={17} className="text-green-500" />,
+  "Filming": <Clapperboard size={17} className="text-bue-500" />,
+  "Fitness": <Dumbbell size={17} className="text-purple-500" />,
+  "Music": <Music size={17} className="text-pink-500" />,
+  "Photography": <Aperture size={17} className="text-blue-500" />,
+};
 
 const Category = ({ category }: { category: Category[] }) => {
   return (
@@ -26,6 +34,7 @@ const Category = ({ category }: { category: Category[] }) => {
             key={items.id}
             title={
               <div className="flex items-center space-x-2">
+                {IconMap[items.name]}
                 <span>{items.name}</span>
               </div>
             }
